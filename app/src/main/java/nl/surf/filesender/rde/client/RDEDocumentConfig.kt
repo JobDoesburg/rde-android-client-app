3e898e559242675951342ebbb54e8c7bb74ccca3
Job Doesburg <job.doesburg@surf.nl>
2022-10-25 13:54:30 +0200

    Fix bugs

--- app/src/main/java/nl/surf/filesender/rde/client/RDEDocumentConfig.kt
+++ app/src/main/java/nl/surf/filesender/rde/client/RDEDocumentConfig.kt
@@ -6,6 +6,6 @@ object RDEDocumentConfig {
     const val MAX_BLOCK_SIZE = PassportService.DEFAULT_MAX_BLOCKSIZE
     const val TRANCEIVE_LENGTH_FOR_SECURE_MESSAGING = PassportService.NORMAL_MAX_TRANCEIVE_LENGTH
     const val USE_PACE_INSTEAD_OF_BAC = true
-    const val SFI_ENABLED = false
-    const val DG_ID_FOR_RDE = PassportService.EF_DG14.toInt() // The datagroup to use for RDE, probably you want DG14 because it does not contain privacy sensitive data
+    const val SFI_ENABLED = true
+    const val DG_ID_FOR_RDE = PassportService.SFI_DG14.toInt() // The datagroup to use for RDE, probably you want DG14 because it does not contain privacy sensitive data
 }
